d7079ce1a60d50277c4c6900f91c96f00d489c86
LuanOliveira27 <luangabrielno2@gmail.com>
2025-08-14 20:28:14 -0300

    Commit Inicial

---
+++ src/ola_mundo/Main.java
@@ -0,0 +1,12 @@
+package ola_mundo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//PARA ENVIAR QUALQUER MENSAGEM PARA O CONSOLE É NECESSARIO CRIAR O COMANDO (System.out.print("......");
+		//PARA PULAR LINHA É NECESSARIO ACRECENTAR O (ln) NO FINAL DO COMANDO (System.out.println)
+		 
+		System.out.println("Olá Pessoal");
+		System.out.print("Sejam Bem-Vindos!!");
+	}
+}
